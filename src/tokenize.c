81170dfd3d1bca2e7c2dddfb249579d0bf13deb9
pnguyen- <152603632+K-Pascal@users.noreply.github.com>
2024-03-12 20:28:10 +0100

    fix tokenize()
    * double quotes are not anymore closed by single quotes and vice versa
    * re-assign variable token with the function store_and_create_token()

--- src/tokenize.c
+++ src/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 15:39:44 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/03/11 19:54:47 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/03/12 20:27:05 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,12 @@
 #include "parser.h"
 
 #define OPERATOR_TOKENS "<>|"
-#define QUOTES_CHARS "\"\'"
+#define QUOTES_CHARS "\"'"
+
+#define NO_QUOTE 0
+#define SG_QUOTE 1
+#define DB_QUOTE 2
+
 
 int	add_to_list(t_list **tokens, t_token *token)
 {
@@ -34,14 +39,16 @@ int	add_to_list(t_list **tokens, t_token *token)
 	return (1);
 }
 
-t_token	*store_token(t_list **tokens, t_token *token, char line[], int len)
+t_token	*store_and_create_token(t_list **tokens, t_token *token,
+		char line[], int len)
 {
-	token->data = ft_substr(line, 0, i);
+	token->data = ft_substr(line, 0, len);
 	if (!add_to_list(tokens, token))
 		return (NULL);
 	token = ft_calloc(1, sizeof(t_token));
 	if (token == NULL)
-		perror("tokenize():ft_calloc()");
+		perror("store_and_create_token():ft_calloc()");
+	token->type = T_NONE;
 	return (token);
 }
 
@@ -63,6 +70,7 @@ int	tokenize(t_list **tokens, char line[])
 	quoted = 0;
 	while (1)
 	{
+		// RULE #1
 		if (line[i] == '\0')
 		{
 			token->data = ft_substr(line, 0, i + 1);
@@ -70,8 +78,9 @@ int	tokenize(t_list **tokens, char line[])
 				return (0);
 			break ;
 		}
-		if (token->type & T_OPERATOR && !quoted)
+		if (token->type & T_OPERATOR)
 		{
+			// RULE #2
 			if (ft_strchr(OPERATOR_TOKENS, line[i]) != NULL)
 			{
 				if ((token->type & T_REDIRECT_OUTPUT))
@@ -107,57 +116,58 @@ int	tokenize(t_list **tokens, char line[])
 				}
 				else if ((token->type & T_PIPE))
 				{
-					if (store_token(tokens, token, line, i) == NULL)
+					token = store_and_create_token(tokens, token, line, i);
+					if (token == NULL)
 						return (0);
-					token->type = T_WORD;
 					line += i;
 					i = 0;
 					continue ;
 				}
 				i++;
+				continue ;
 			}
+			// RULE #3
 			else
 			{
-				token->data = ft_substr(line, 0, i);
-				if (!add_to_list(tokens, token))
-					return (0);
-				token = ft_calloc(1, sizeof(t_token));
+				token = store_and_create_token(tokens, token, line, i);
 				if (token == NULL)
-				{
-					perror("tokenize():ft_calloc()");
 					return (0);
-				}
-				if (ft_isspace(line[i]))
-					token->type = T_NONE;
-				else
-					token->type = T_WORD;
-				line += i;
-				i = 0;
-			}
-			continue ;
-		}
-		if (ft_strchr(QUOTES_CHARS, line[i]) != NULL)
-		{
-			quoted ^= 1;
-			i++;
-			continue ;
-		}
-		if (!quoted && !(token->type & T_OPERATOR) && ft_strchr(OPERATOR_TOKENS, line[i]) != NULL)
-		{
-			if (token->type & T_WORD)
-			{
-				token->data = ft_substr(line, 0, i);
-				if (!add_to_list(tokens, token))
-					return (0);
-				token = ft_calloc(1, sizeof(t_token));
-				if (token == NULL)
-				{
-					perror("tokenize():ft_calloc()");
-					return (0);
-				}
 				line += i;
 				i = 0;
 				continue ;
+			}
+		}
+		// RULE #4
+		if (ft_strchr(QUOTES_CHARS, line[i]) != NULL)
+		{
+			if (line[i] == '\'')
+			{
+				if (quoted == NO_QUOTE)
+					quoted = SG_QUOTE;
+				else if (quoted == SG_QUOTE)
+					quoted = NO_QUOTE;
+			}
+			else if (line[i] == '"')
+			{
+			   if (quoted == NO_QUOTE)
+				   quoted = DB_QUOTE;
+			   else if (quoted == DB_QUOTE)
+					quoted = NO_QUOTE;
+			}
+			token->type = T_WORD;
+			i++;
+			continue ;
+		}
+		// RULE #6
+		if (quoted == NO_QUOTE && ft_strchr(OPERATOR_TOKENS, line[i]) != NULL)
+		{
+			if (token->type & T_WORD)
+			{
+				token = store_and_create_token(tokens, token, line, i);
+				if (token == NULL)
+					return (0);
+				line += i;
+				i = 0;
 			}
 			token->type = T_OPERATOR;
 			if (line[i] == '>')
@@ -169,42 +179,39 @@ int	tokenize(t_list **tokens, char line[])
 			i++;
 			continue ;
 		}
-		if (!quoted && ft_isspace(line[i]))
+		// RULE #7
+		if (quoted == NO_QUOTE && ft_isspace(line[i]))
 		{
-			if (i != 0)
+			if (token->type != T_NONE)
 			{
-				token->data = ft_substr(line, 0, i);
-				if (!add_to_list(tokens, token))
-					return (0);
-				token = ft_calloc(1, sizeof(t_token));
+				token = store_and_create_token(tokens, token, line, i);
 				if (token == NULL)
-				{
-					perror("tokenize():ft_calloc()");
 					return (0);
-				}
-				line += i;
+				line += i + 1;
 				i = 0;
 			}
 			else
-			{
 				line++;
-				i = 0;
-			}
 			token->type = T_NONE;
 			continue ;
 		}
+		// RULE #8
 		if (token->type & T_WORD)
 		{
 			i++;
 			continue ;
 		}
+		// RULE #9
 		if (line[i] == '#')
 		{
-			free(token);
+			token->data = ft_substr(line, 0, i + 1);
+			if (!add_to_list(tokens, token))
+				return (0);
 			break ;
 		}
+		// RULE #10
 		token->type = T_WORD;
 		i++;
 	}
-	return (!quoted);
+	return (quoted == NO_QUOTE);
 }
