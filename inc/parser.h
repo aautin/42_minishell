e9be088f0e33380de40fc9dacabfde82c8d1c9e8
pnguyen- <152603632+K-Pascal@users.noreply.github.com>
2024-03-13 20:54:38 +0100

    add more flags for more precise operators

--- inc/parser.h
+++ inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 15:30:29 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/03/11 19:35:46 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/03/13 20:44:21 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ typedef enum e_token_type
 	T_NONE = 0u,
 	T_WORD = (1u << 0),
 	T_OPERATOR = (1u << 1),
-	T_REDIRECT_OUTPUT = (1u << 2),
-	T_REDIRECT_INPUT = (1u << 3),
-	T_REDIRECT_HERE_DOC = (1u << 4),
-	T_REDIRECT_APPEND = (1u << 5),
-	T_PIPE = (1u << 6)
+	T_REDIRECT_OPERATOR = (1u << 2),
+	T_CONTROL_OPERATOR = (1u << 3),
+	T_REDIRECT_OUTPUT = (1u << 4),
+	T_REDIRECT_INPUT = (1u << 5),
+	T_REDIRECT_HERE_DOC = (1u << 6),
+	T_REDIRECT_APPEND = (1u << 7),
+	T_PIPE = (1u << 8)
 }	t_token_type;
 
 typedef struct s_token
