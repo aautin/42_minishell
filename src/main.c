e5a06f5d6cc7fcaa323f7d9630749740dbbfaa57
pnguyen- <152603632+K-Pascal@users.noreply.github.com>
2024-03-20 14:48:36 +0100

    make main() being able to test things

--- src/main.c
+++ src/main.c
@@ -6,15 +6,55 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 15:07:01 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/03/07 15:52:41 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/03/20 14:47:01 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int	main(void)
+#include "libft/libft.h"
+
+char	*ask_input(char const prompt[])
 {
-	printf("Initial Release !\n");
+	char	*line;
+	char	*line_with_nl;
+
+	if (isatty(STDIN_FILENO))
+	{
+		line = readline(prompt);
+		if (line == NULL)
+			return (NULL);
+		line_with_nl = ft_strjoin(line, "\n");
+		if (line_with_nl == NULL)
+			perror("ask_input():ft_strjoin()");
+		free(line);
+	}
+	else
+		line_with_nl = get_next_line(STDIN_FILENO);
+	return (line_with_nl);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	char	*line;
+
+	while (1)
+	{
+		line = ask_input("minishell> ");
+		if (line == NULL)
+			break ;
+		*ft_strrchr(line, '\n') = '\0';
+		if (*line != '\0')
+			add_history(line);
+		free(line);
+	}
+	rl_clear_history();
 	return (EXIT_SUCCESS);
 }
