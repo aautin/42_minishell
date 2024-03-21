06684ae92c3fec178fe0bd9a19e54574a2f0b525
pnguyen- <152603632+K-Pascal@users.noreply.github.com>
2024-03-21 18:24:30 +0100

    update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 15:07:01 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/03/21 18:15:00 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/03/21 18:22:20 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,33 @@
 
 #include "libft/libft.h"
 
+#include "execution.h"
+#include "parser.h"
 #include "utils.h"
-
-int	execute_line(t_list *tokens, char **envp);
 
 int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
-	(void)envp;
 	char	*line;
+	int		status;
+	t_list	*tokens;
 
 	while (1)
 	{
+		tokens = NULL;
 		line = ask_input("minishell> ");
 		if (line == NULL)
 			break ;
-		*ft_strrchr(line, '\n') = '\0';
 		if (*line != '\0')
 			add_history(line);
+		status = tokenize(&tokens, line);
 		free(line);
-		if (!status || tokens == NULL)
+		if (!status && tokens != NULL)
 		{
 			bad_node = verify_tokens(tokens);
 			if (bad_node != NULL)
-				printf("Unexpected token '%s'\n", (char *)((t_token *)bad_node->content)->data);
+				printf("Unexpected token '%s'\n", ((t_token *)bad_node->content)->data);
 			execute_line(tokens, envp);
 		}
 		else if (status == 1)
