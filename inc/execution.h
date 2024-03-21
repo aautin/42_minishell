e4eeda453366e5a20b2ab92bdde9b527ea16a937
pnguyen- <152603632+K-Pascal@users.noreply.github.com>
2024-03-21 18:22:55 +0100

    add execute_line() prototype to its header file

--- inc/execution.h
+++ inc/execution.h
@@ -6,11 +6,15 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 10:18:52 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/03/19 10:19:11 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/03/21 18:16:40 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTION_H
 # define EXECUTION_H
+
+#include "libft/libft.h"
+
+int	execute_line(t_list *tokens, char **envp);
 
 #endif
