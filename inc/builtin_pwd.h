ba859f29c02945a349fa66d88855b4007f93071a
aautin <ezagof@gmail.com>
2024-04-11 15:32:25 +0200

    I better felt not confident about it haha, now seems better
    * fixed the perror msg and move the .h includes into the .c

--- inc/builtin_pwd.h
+++ inc/builtin_pwd.h
@@ -6,16 +6,12 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 14:38:37 by aautin            #+#    #+#             */
-/*   Updated: 2024/04/11 15:28:10 by aautin           ###   ########.fr       */
+/*   Updated: 2024/04/11 15:31:51 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_PWD_H
 # define BUILTIN_PWD_H
-
-# include <unistd.h>
-# include <stdio.h>
-# include <linux/limits.h>
 
 int	builtin_pwd(void);
 
--- src/builtin_pwd.c
+++ src/builtin_pwd.c
@@ -6,9 +6,13 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 14:36:48 by aautin            #+#    #+#             */
-/*   Updated: 2024/04/11 15:26:44 by aautin           ###   ########.fr       */
+/*   Updated: 2024/04/11 15:32:22 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+# include <linux/limits.h>
+# include <stdio.h>
+# include <unistd.h>
 
 #include "builtin_pwd.h"
 
@@ -18,12 +22,12 @@ int	builtin_pwd(void)
 
 	if (getcwd(buff, PATH_MAX) == NULL)
 	{
-		perror("builtin_pwd():getcwd()");
+		perror("pwd():getcwd()");
 		return (1);
 	}
 	if (printf("%s\n", buff) == -1)
 	{
-		perror("builtin_pwd():printf()");
+		perror("pwd():printf()");
 		return (1);
 	}
 	return (0);
