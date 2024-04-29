1f44f5835aa80f5ecf7e0d593f671056458537d4
aautin <ezagof@gmail.com>
2024-04-29 20:34:34 +0200

    update

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 17:35:17 by aautin            #+#    #+#             */
-/*   Updated: 2024/04/28 18:51:48 by aautin           ###   ########.fr       */
+/*   Updated: 2024/04/29 19:49:41 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,75 @@
 
 #include "libft/libft.h"
 
-static char *match_cd_paths(char *cd_paths, )
+#define NOT_DOT		0
+#define ONE_DOT		1
+#define TWO_DOT		2
+
+static int	get_pathmode(char arg[])
+{
+	if (arg[0] == '.' && arg[1] == '.')
+		return (TWO_DOT);
+	if (arg[0] == '.')
+		return (ONE_DOT);
+	return (NOT_DOT);
+}
+
+static char	*build_path(char *first_part, char *second_part)
 {
 	
+}
+
+static char	*get_cdpath(char **cdpaths, char arg[])
+{
+	int		i;
+	char	*path;
+
+	i = 0;
+	if (cdpaths == NULL)
+		return (NULL);
+	while (cdpaths[i])
+	{
+		path = build_path(cdpaths[i], arg);
+		if (path != NULL && access())
+		free(cdpaths[i])
+		i++;
+	}
+	while (cdpaths[i])
+		free(cdpaths[i++]);
+	free(cdpaths);
 	return (NULL);
 }
 
-int	builtin_cd(char **argv, t_list **envp) {
-	char	*env_val;
-	char	*dir_operand;
+static int	execute(char curpath[], t_list **envp)	// from 
+{
+	
+	return (0);
+}
 
+int	builtin_cd(char **argv, t_list **envp)
+{
+	char	*env_val;
+	char	*curpath;
+
+	curpath = NULL;
 	if (argv[1] == NULL)
 	{
 		env_val = ft_getenv(*envp, "HOME");
-		if (env_val == NULL || env_val[0] == '\0')
+		if (env_val == NULL || env_val[0] == '\0')	// 1.
 			return (0);
-		dir_operand = env_val;
+		curpath = env_val;							// 2.
 	}
-	else if (argv[1][0] == '/')
-		dir_operand = argv[1];
-	else
+	else if (argv[1][0] == '/')						// 3.
+		curpath = argv[1];
+	else if (get_pathmode(argv[1]) == NOT_DOT)		// 5.
 	{
 		env_val = ft_getenv(*envp, "CDPATH");
 		if (env_val != NULL)
-		{
-			dir_operand = 
-		}
+			curpath = get_cdpath(ft_split(env_val, ':'), argv[1]);
+		if (curpath == NULL)
+			curpath = argv[1];
 	}
-	return (0);
+	else											// 4.
+		curpath = argv[1];
+	return (execute(curpath, envp));
 }
