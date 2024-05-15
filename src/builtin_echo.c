520061b0394eb660341520378d969c49dc6c2e12
aautin <ezagof@gmail.com>
2024-05-15 16:42:45 +0200

    explicit code style

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 12:20:31 by aautin            #+#    #+#             */
-/*   Updated: 2024/04/19 18:21:40 by aautin           ###   ########.fr       */
+/*   Updated: 2024/05/15 16:42:30 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@
 #define NORMAL_MODE 0
 #define NO_NEWLINE_MODE 1
 
-static int	get_status(char *arg)
+static int	get_status(char const arg[])
 {
 	if (*arg != '-')
 		return (NORMAL_MODE);
 	arg++;
 	if (*arg == '\0')
 		return (NORMAL_MODE);
-	while ('n' == *arg)
+	while (*arg == 'n')
 		arg++;
 	if (*arg == '\0')
 		return (NO_NEWLINE_MODE);
@@ -36,25 +36,23 @@ static int	get_status(char *arg)
 static char	*join_string_argv(char **argv, char format)
 {
 	char	*output;
-	int		len;
-	int		i;
+	size_t	len;
+	size_t	i;
 
 	i = 0;
 	len = 0;
-	while (argv[i])
+	while (argv[i] != NULL)
 		len += ft_strlen(argv[i++]);
 	len = len + i + (format == NORMAL_MODE);
 	output = malloc(len * sizeof(char));
 	if (output == NULL)
-	{
-		perror("join_string_argv():malloc()");
-		return (NULL);
-	}
+		return (perror("join_string_argv():malloc()"), NULL);
 	output[0] = '\0';
-	while (*argv)
+	i = 0;
+	while (argv[i])
 	{
-		ft_strlcat(output, *(argv++), len);
-		if (*argv)
+		ft_strlcat(output, argv[i++], len);
+		if (argv[i])
 			ft_strlcat(output, " ", len);
 	}
 	if (format == NORMAL_MODE)
@@ -67,13 +65,10 @@ int	builtin_echo(char **argv)
 	char	format;
 	char	*output;
 
-	if (argv[1] == NULL)
+	if (*(argv + 1) == NULL)
 	{
 		if (write(1, "\n", 1) == -1)
-		{
-			perror("echo():write()");
-			return (1);
-		}
+			return (perror("echo():write()"), 1);
 		return (0);
 	}
 	format = NORMAL_MODE;
@@ -90,11 +85,6 @@ int	builtin_echo(char **argv)
 	if (output == NULL)
 		return (1);
 	if (write(1, output, ft_strlen(output)) == -1)
-	{
-		perror("echo():write()");
-		free(output);
-		return (1);
-	}
-	free(output);
-	return (0);
+		return (perror("echo():write()"), free(output), 1);
+	return (free(output), 0);
 }
