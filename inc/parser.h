1c29eccd422bf760475aa8eec1e448fe9d70fa24
aautin <ezagof@gmail.com>
2024-05-17 17:42:50 +0200

    update parser.h

--- inc/parser.h
+++ inc/parser.h
@@ -6,12 +6,14 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 15:30:29 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/04/11 15:39:02 by aautin           ###   ########.fr       */
+/*   Updated: 2024/05/17 17:44:39 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
+
+# include "libft/libft.h"
 
 # define NO_QUOTE 0
 # define SG_QUOTE 1
@@ -41,7 +43,8 @@ typedef struct s_token
 void	unquote(char data[]);
 int		expansion(t_token *token, t_list *envp,
 			int ignore_quotes, int exit_status);
-int		parse_token(t_token *token, t_list *envp, int exit_status);
-int		tokenize(t_list **tokens, char line[]);
+int		parse_token(t_token *token, t_list *envp, int exit_status, int ignore_quotes);
+int		tokenize(t_list **tokens, char const line[]);
+t_list	*verify_tokens(t_list *tokens);
 
 #endif
