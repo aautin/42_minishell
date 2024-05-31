8475e39c1ac71c708b16887488423fd1e51fe423
aautin <ezagof@gmail.com>
2024-05-31 17:53:51 +0200

    fix expansion behavior when not found

--- src/parse_utils.c
+++ src/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 16:36:12 by aautin            #+#    #+#             */
-/*   Updated: 2024/05/30 20:49:19 by aautin           ###   ########.fr       */
+/*   Updated: 2024/05/31 17:52:33 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ size_t	pathname_len(char const pathname[])
 	size_t	len;
 
 	len = 0;
+	if (pathname[0] == '?')
+		return (1);
 	if (!ft_isalpha(pathname[0]) && pathname[0] != '_')
 		return (0);
 	while (ft_isalnum(pathname[len]) || pathname[len] == '_')
@@ -36,7 +38,7 @@ int	expansion(t_list *component, t_list *envp,
 	char	*data;
 	char	*env;
 
-	data = (char *) component->content;
+	data = component->content;
 	if (data[1] == '?')
 	{
 		env = ft_itoa(exit_status);
@@ -47,14 +49,14 @@ int	expansion(t_list *component, t_list *envp,
 	{
 		env = ft_getenv(envp, &data[1]);
 		if (env != NULL)
-		{
 			env = ft_strdup(env);
-			if (env == NULL)
-				return (perror("expansion():ft_strdup()"), 1);
-		}
+		else if (data[1] != '\0')
+			env = ft_strdup("");
+		else
+			return (0);
+		if (env == NULL)
+			return (perror("expansion():ft_strdup()"), 1);
 	}
-	if (env == NULL)
-		return (0);
 	free(component->content);
 	component->content = env;
 	return (0);
